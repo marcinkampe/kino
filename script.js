e771e8109d699d0afbb563f7954a0bae2bffe39a
Marcin Kampe <m.kampe.2019@gmail.com>
2022-11-17 19:25:42 +0100

    dokończenie strony

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 // function jaki_film() {
 //     console.log(film);
 // }
+function checkform(form) {
+  // get all the inputs within the submitted form
+  let inputs = document.forms.bilet.getElementsByTagName("input");
+  for (let i = 0; i < inputs.length; i++) {
+    // only validate the inputs that have the required attribute
+    if (inputs[i].hasAttribute("required")) {
+      if (inputs[i].value == "") {
+        // found an empty field that is required
+        alert("Please fill all required fields");
+        return false;
+      }
+    }
+  }
+  return true;
+}
